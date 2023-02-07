2fe06a7564f663dbbb07485ba70348502d2e3948
ratSeraphim <telma.laudobele@accenture.com>
2023-02-07 15:47:23 +0200

    Styled hard-coded home page

--- src/components/RecipeList/RecipeList.jsx
+++ src/components/RecipeList/RecipeList.jsx
@@ -5,7 +5,7 @@ import * as S from "./style";
 
 const RecipeList = () => {
   return (
-    <div>
+    <S.RecipeList>
       <S.Recipe>
         <S.Title>Veggie Stew</S.Title>
         <S.Time>45 minutes to make</S.Time>
@@ -24,7 +24,34 @@ const RecipeList = () => {
         </S.Steps>
         <S.Button>Cook this</S.Button>
       </S.Recipe>
-    </div>
+      <S.Recipe>
+        <S.Title>Veggie Stew</S.Title>
+        <S.Time>45 minutes to make</S.Time>
+        <S.Steps>
+          1. Pre-heat the oven to 200C. 2. Place the carrot, leek and tofu in a
+          large bowl.
+        </S.Steps>
+        <S.Button>Cook this</S.Button>
+      </S.Recipe>
+      <S.Recipe>
+        <S.Title>Greek Salad</S.Title>
+        <S.Time>35 minutes to make</S.Time>
+        <S.Steps>
+          1. Pre-heat the oven to 200C. 2. Place the carrot, leek and tofu in a
+          large bowl.
+        </S.Steps>
+        <S.Button>Cook this</S.Button>
+      </S.Recipe>
+      <S.Recipe>
+        <S.Title>Veggie Stew</S.Title>
+        <S.Time>45 minutes to make</S.Time>
+        <S.Steps>
+          1. Pre-heat the oven to 200C. 2. Place the carrot, leek and tofu in a
+          large bowl.
+        </S.Steps>
+        <S.Button>Cook this</S.Button>
+      </S.Recipe>
+    </S.RecipeList>
   );
 };
 
--- src/components/RecipeList/style.js
+++ src/components/RecipeList/style.js
@@ -1,16 +1,42 @@
 import styled from "styled-components";
 
+export const RecipeList = styled.div`
+  margin: 50px 150px;
+  display: grid;
+  gap: 30px;
+  grid-template-columns: [first] 1fr [second] 1fr [third] 1fr;
+`;
+
 export const Recipe = styled.div`
+  padding: 15px 20px;
   background-color: #fff;
+  box-shadow: 2px 2px 3px #555;
+  &:hover {
+    transform: rotate(5deg);
+  }
 `;
 
 export const Title = styled.h1`
+  text-align: left;
   font-weight: bold;
   font-size: 24px;
 `;
 
-export const Time = styled.h2``;
+export const Time = styled.h2`
+  color: #aaa;
+`;
 
-export const Steps = styled.div``;
+export const Steps = styled.div`
+  margin: 10px;
+`;
 
-export const Button = styled.a``;
+export const Button = styled.a`
+  background-color: #dfdfdf;
+  padding: 5px 15px;
+  border-radius: 2px;
+
+  &:hover {
+    background-color: #555;
+    color: #fff;
+  }
+`;
