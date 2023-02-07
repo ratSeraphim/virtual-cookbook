2cc69b0905a50bf07bd6c799b3ec864a970cb4a8
ratSeraphim <telma.laudobele@accenture.com>
2023-02-07 14:25:20 +0200

    Began project

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+const Navbar = () => {
+  return (
+    <div>
+      <h1>Cook Book</h1>
+      <div>
+        Search:
+        <input type="text"></input>
+      </div>
+      <a href="/create">create recipe</a>
+    </div>
+  );
+};
+
+export default Navbar;
